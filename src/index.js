384f14809e5a3cf5323e494456f573aeef8c3fd0
Antonio martinez <josanmaes@gmail.com>
2025-05-31 17:44:34 -0600

    Add cors

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import carRoutes from './routes/carRoutes.js'
 dotenv.config()
 const app = express()
 
-app.use(cors())
+app.use(cors({ origin: '*' }))
 app.use(express.json())
 app.use('/uploads', express.static('uploads'))
 app.use((req, res, next) => {
